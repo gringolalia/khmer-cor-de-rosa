9d61f4d21b46f9dfd3fd63a0e64f487f9f3e6924
Colin Brayton <gringolalia@gmail.com>
2019-08-14 17:49:39 -0300

    deploy

---
+++ precache-manifest.dfd57ced13e608cbfd3dc27b40ab2db9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "21393b2c1bcf915887c5",
+    "url": "/khmer-cor-de-rosa/js/about.6c97fb3b.js"
+  },
+  {
+    "revision": "aff962120b295fe94948",
+    "url": "/khmer-cor-de-rosa/css/app.02685768.css"
+  },
+  {
+    "revision": "aff962120b295fe94948",
+    "url": "/khmer-cor-de-rosa/js/app.c40a8c82.js"
+  },
+  {
+    "revision": "4926e86e285f82d36fad",
+    "url": "/khmer-cor-de-rosa/css/chunk-vendors.c1690696.css"
+  },
+  {
+    "revision": "4926e86e285f82d36fad",
+    "url": "/khmer-cor-de-rosa/js/chunk-vendors.2dcaa7f7.js"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/khmer-cor-de-rosa/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "de7882df4bc1d2f7d39837aae0ef6432",
+    "url": "/khmer-cor-de-rosa/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/khmer-cor-de-rosa/robots.txt"
+  }
+];
